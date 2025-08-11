dc1bc24dcc66fb808c2f2821f326cb693a3d7270
ali <zboom0ne01@gmail.com>
2025-08-11 09:34:25 +0330

    update c

--- src/components/ObjectDetectionComponent.tsx
+++ src/components/ObjectDetectionComponent.tsx
@@ -822,7 +822,7 @@ const ObjectTrackingComponent: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col items-center p-4 bg-gray-100 min-h-screen" style={{ direction: 'rtl', fontFamily: 'Vazirmatn, sans-serif' }}>
+        <div className="flex justify-center flex-col items-center p-4 bg-gray-100 min-h-screen" style={{ direction: 'rtl', fontFamily: 'Vazirmatn, sans-serif' }}>
             <motion.div
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
